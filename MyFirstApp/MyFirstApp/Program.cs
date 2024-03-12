cc52e997b5791ac7673e1c30ae067d5c2065aa23
s27179 <s27179@pjwstk.edu.pl>
2024-03-12 17:00:09 +0100

    Add GetAverage method

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -8,3 +8,15 @@ int b = 2;
 Console.WriteLine("Hello");
 
 int c = 3;
+
+static double GetAverage(int[] numbers)
+{
+    double sum = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        sum += numbers[i];
+    }
+
+    return sum / numbers.Length;
+}
+
